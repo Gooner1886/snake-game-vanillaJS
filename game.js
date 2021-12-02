cb6b7416cbb1e94c657f57a8a18b22457eb02e94
Siddharth Sarma <siddharthgsarma5@gmail.com>
2021-12-02 12:36:51 +0530

    added snake.js

--- game.js
+++ game.js
@@ -1,25 +1,26 @@
-let lastRenderTime = 0
-const SNAKE_SPEED  = 2;
+let lastRenderTime = 0;
+import { SNAKE_SPEED } from "./snake";
+import { update as updateSnake } from "./snake";
+import { draw as drawSnake } from "./snake";
 
 const main = (currentTime) => {
-    window.requestAnimationFrame(main)
-    let secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-    if(secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+  window.requestAnimationFrame(main);
+  let secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
 
+  
+  lastRenderTime = currentTime;
 
-    console.log('Render');
-    lastRenderTime = currentTime;
-
-    update() // updates all logic for game
-    draw() // draws the updated logic to the screen
-}
+  update(); // updates all logic for game
+  draw(); // draws the updated logic to the screen
+};
 
 window.requestAnimationFrame(main);
 
 const update = () => {
-    
-}
+    updateSnake()
+};
 
 const draw = () => {
-
-}
+    drawSnake()
+};
---
+++ snake.js
@@ -0,0 +1,10 @@
+export const SNAKE_SPEED  = 2;
+
+
+export const update = () => {
+    console.log("Update Snake");
+}
+
+export const draw = () => {
+    console.log("Draw Snake");
+}
